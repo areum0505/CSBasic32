62d054d919736df59189567a7ef70b422371888e
areum0505 <kimarum2003@gmail.com>
2021-07-01 09:22:16 +0900

    #26 클래스 메서드 실습

--- CSBasic5/Program.cs
+++ CSBasic5/Program.cs
@@ -8,6 +8,20 @@ namespace CSBasic5
 {
     class Program
     {
+        public int instanceVariable = 10;
+        public int instanceMethod()
+        {
+            return 0;
+        }
+
+        class MyMath
+        {
+            public static int Abs(int input)
+            {
+                return (input < 0 ? -input : input);
+            }
+        }
+
         class Test
         {
             // Method Signature
@@ -51,6 +65,21 @@ namespace CSBasic5
 
             Test test = new Test();
             Console.WriteLine(test.Sum(1, 100));
+
+            Console.WriteLine();
+
+            Console.WriteLine(MyMath.Abs(52));
+            Console.WriteLine(MyMath.Abs(-52));
+
+            Console.WriteLine();
+
+            // 클래스메서드에서는 인스턴스 변수, 메서드 접근 불가
+            // 객체를 만들어 접근해야한다.
+            //Console.WriteLine(instanceVariable);
+            //instanceMethod();
+            Program p = new Program();
+            Console.WriteLine(p.instanceVariable);
+            Console.WriteLine(p.instanceMethod());
         }
     }
 }
