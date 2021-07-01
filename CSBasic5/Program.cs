468f856bc6f69ffe3daf838443446fa03c1f4f28
areum0505 <kimarum2003@gmail.com>
2021-07-01 09:51:19 +0900

    #27 접근제한자(public, private 예제)

--- CSBasic5/Program.cs
+++ CSBasic5/Program.cs
@@ -8,6 +8,8 @@ namespace CSBasic5
 {
     class Program
     {
+        int someModifier = 0;   // private
+
         public int instanceVariable = 10;
         public int instanceMethod()
         {
@@ -32,6 +34,10 @@ namespace CSBasic5
 
         class Test
         {
+            public void TestMethod()
+            {
+                Program.Main(new string[] { "" });
+            }
             // Method Signature
             public int Sum(int min, int max)
             {
@@ -96,6 +102,10 @@ namespace CSBasic5
             Console.WriteLine(MyMath.Abs(-273));
             Console.WriteLine(MyMath.Abs(52.073));
             Console.WriteLine(MyMath.Abs(299999999999));
+
+            Console.WriteLine();
+
+
         }
     }
 }
