02656ced41b560433bb45cc1a8648386a7df1aac
areum0505 <kimarum2003@gmail.com>
2021-08-24 12:13:57 +0900

    하이딩과 오버라이딩 비교 실습

--- CSBasic6/Program.cs
+++ CSBasic6/Program.cs
@@ -8,8 +8,105 @@ namespace CSBasic6
 {
     class Program
     {
+       
+        class Animal
+        {
+            public void Eat()
+            {
+                Console.WriteLine("냠냠");
+            }
+            public virtual void Eat2()
+            {
+                Console.WriteLine("냠냠");
+            }
+        }
+        class Cat : Animal
+        {
+            public new void Eat()
+            {
+                Console.WriteLine("야옹");
+            }
+            public override void Eat2()
+            {
+                Console.WriteLine("야옹");
+            }
+        }
+        class Dog : Animal
+        {
+            public new void Eat() {
+                Console.WriteLine("멍멍");
+            }
+            public override void Eat2()
+            {
+                Console.WriteLine("멍멍");
+            }
+        }
+
+        class Parent
+        {
+            public int variable = 273;
+            public void Method()    // 하이딩
+            {
+                Console.WriteLine("부모 메서드");
+            }
+            public virtual void Method2()   // 오버라이드
+            {
+                Console.WriteLine("부모 메서드");
+            }
+        }
+        class Child : Parent
+        {
+            public new string variable = "hiding";
+            public new void Method()
+            {
+                Console.WriteLine("자식 메서드");
+            }
+            public override void Method2()  
+            {
+                Console.WriteLine("자식 메서드");
+            }
+        }
+
         static void Main(string[] args)
         {
+            Child child = new Child();
+            Console.WriteLine(child.variable);
+            Console.WriteLine(((Parent)child).variable);
+            Console.WriteLine();
+
+            child.Method();
+            ((Parent)child).Method();
+            Console.WriteLine();
+
+            child.Method2();
+            ((Parent)child).Method2();
+
+
+            Console.WriteLine("---------------");
+
+
+            List<Animal> animals = new List<Animal>()
+            {
+                new Dog(), new Cat(), new Dog(), new Cat(),
+                new Dog(), new Cat(), new Dog(), new Cat()
+            };
+            foreach (var item in animals)
+            {
+                item.Eat();
+            }
+
+            Console.WriteLine();
+
+            List<Animal> animals2 = new List<Animal>()
+            {
+                new Dog(), new Cat(), new Dog(), new Cat(),
+                new Dog(), new Cat(), new Dog(), new Cat()
+            };
+            foreach (var item in animals2)
+            {
+                item.Eat2();
+            }
+
         }
     }
 }
