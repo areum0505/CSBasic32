0bde7b6fcf37acaeff28a610b79475f8327755e4
areum0505 <kimarum2003@gmail.com>
2021-09-02 10:19:21 +0900

    파일입출력 예제

--- CSBasic8/Program.cs
+++ CSBasic8/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +9,19 @@ namespace CSBasic8
 {
     class Program
     {
-        
+        class Parent { }
+        class Child : Parent, IDisposable, IComparable
+        {
+            public int CompareTo(object obj)
+            {
+                throw new NotImplementedException();
+            }
+
+            public void Dispose()
+            {
+                throw new NotImplementedException();
+            }
+        }
         class TestClass : IBasic
         {
             public int TestProperty 
@@ -74,6 +87,38 @@ namespace CSBasic8
 
             IBasic basic = new TestClass();
             basic.TestProperty = 3;
+
+            Console.WriteLine();
+
+            Child child = new Child();
+            Parent childParent = new Child();
+            IDisposable childDisposable = new Child();
+            IComparable childComparable = new Child();
+
+            Console.WriteLine();
+
+            // 파일입출력
+            File.WriteAllText(@"C:\Users\Mirim\Desktop\test.txt", "안녕하세요");
+            Console.WriteLine(File.ReadAllText(@"C:\Users\Mirim\Desktop\test.txt"));
+
+            using(StreamWriter writer = new StreamWriter(@"C:\Users\Mirim\Desktop\test2.txt"))
+            {
+                writer.WriteLine("안녕하세요");
+                writer.WriteLine("StreamWriter");
+                writer.WriteLine("사용");
+                for (int i = 0; i < 10; i++)
+                {
+                    writer.WriteLine("반복문 - " + i);
+                }
+            }
+            using(StreamReader reader = new StreamReader(@"C:\Users\Mirim\Desktop\test2.txt"))
+            {
+                string line;
+                while((line = reader.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                }
+            }
         }
     }
 }
