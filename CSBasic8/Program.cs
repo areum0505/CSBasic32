9a26d0aa4838f312b6efef380c8e7669e3f43ad8
areum0505 <kimarum2003@gmail.com>
2021-09-02 09:44:30 +0900

    IDisposable 인터페이스 예제

--- CSBasic8/Program.cs
+++ CSBasic8/Program.cs
@@ -6,25 +6,33 @@ using System.Threading.Tasks;
 
 namespace CSBasic8
 {
-    class Product : IComparable
-    {
-        public string Name { get; set; }
-        public int Price { get; set; }
-
-        public int CompareTo(object obj)
-        {
-            return Price.CompareTo((obj as Product).Price);
-        }
-
-        public override string ToString()
-        {
-            return Name + " : " + Price + "원";
-        }
-    }
     class Program
     {
+        class Dummy : IDisposable
+        {
+            public void Dispose()
+            {
+                Console.WriteLine("리소스가 해제되었습니다.");
+            }
+        }
+        class Product : IComparable
+        {
+            public string Name { get; set; }
+            public int Price { get; set; }
+
+            public int CompareTo(object obj)
+            {
+                return Price.CompareTo((obj as Product).Price);
+            }
+
+            public override string ToString()
+            {
+                return Name + " : " + Price + "원";
+            }
+        }
         static void Main(string[] args)
         {
+            // IComparable
             List<Product> list = new List<Product>()
             {
                   new Product() { Name="고구마", Price=1500},
@@ -38,6 +46,14 @@ namespace CSBasic8
             foreach (var item in list)
             {
                 Console.WriteLine(item);
+            }
+
+            Console.WriteLine();
+
+            // IDisposable
+            using (Dummy d = new Dummy())
+            {
+                Console.WriteLine("뭔가 했습니다.");
             }
         }
     }
